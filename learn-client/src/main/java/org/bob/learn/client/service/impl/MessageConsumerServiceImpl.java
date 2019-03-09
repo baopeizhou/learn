2fbb6f7093a3c0e8be07913ac661a3182f956ae7
baopeizhou <baopeizhou@cmhi.chinamobile.com>
2019-03-09 11:29:15 +0800

    asdfsd

---
+++ learn-client/src/main/java/org/bob/learn/client/service/impl/MessageConsumerServiceImpl.java
@@ -0,0 +1,18 @@
+package org.bob.learn.client.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bob.learn.client.common.dto.Message;
+import org.bob.learn.client.service.MessageConsumerService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class MessageConsumerServiceImpl implements MessageConsumerService {
+
+    @KafkaListener(id = "1",topics = "learn")
+    @Override
+    public void receive(Message message) {
+        log.info(String.valueOf(message));
+    }
+}
