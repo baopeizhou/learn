6c531130691856397cd6f2badcafdd80459f6f30
baopeizhou <baopeizhou@126.com>
2019-07-06 20:43:21 +0800

    sdrswd Signed-off-by: baopeizhou <baopeizhou@126.com>

--- learn-client/target/classes/h2/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `device_online` (
-                              `mac` varchar(12) NOT NULL,
-                              `dmac` varchar(12) NOT NULL,
-                              `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                              UNIQUE INDEX `mac_dmac`(`mac`, `dmac`) USING BTREE
-);
